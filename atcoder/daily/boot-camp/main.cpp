5b1578405302677c1d28adcd6c8959ee10c54253
sakasu <s214229y@st.go.tuat.ac.jp>
2022-09-18 21:10:18 +0900

    abc269

---
+++ atcoder/daily/boot-camp/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+#include <deque>
+using namespace std;
+// #define rep(i,n) for(ll i=0; i<(n); ++i)
+#define Out(x) cout << x << endl
+#define Yes "Yes"
+#define No "No"
+#define Ans cout << ans << endl
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define rrep(i, a, b) for (int i = a; i >= b; --i)
+#define fore(i, a) for (auto &i : a)
+using ll = long long;
+using P = pair<int, int>;
+// const ll mod = 10e9+7;
+
+
+int main(){
+ll n; cin>>n;
+    cout << (n-1)*n/2 << endl;
+}
