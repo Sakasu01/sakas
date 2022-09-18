6c5228215992ccd88394db7e9974212a26bade0e
sakasu <s214229y@st.go.tuat.ac.jp>
2022-09-18 21:30:06 +0900

    dx,dyの扱い

--- atcoder/daily/boot-camp/main.cpp
+++ atcoder/daily/boot-camp/main.cpp
@@ -20,8 +20,30 @@ using ll = long long;
 using P = pair<int, int>;
 // const ll mod = 10e9+7;
 
+int dx[3]={-1,0,1}, dy[3]={-1,0,1};
+
 
 int main(){
-ll n; cin>>n;
-    cout << (n-1)*n/2 << endl;
+    int h,w; cin>>h>>w;
+    vector<vector<char>> table(h, vector<char>(w));
+    rep(i,0,h){rep(j,0,w){
+        cin >> table[i][j];
+    }}
+    rep(i,0,h){rep(j,0,w){
+        int cnt=0;
+        if(table[i][j] == '#') continue;
+        rep(k,0,3){
+            rep(l,0,3){
+                int x = j + dx[k];
+                int y = i + dy[l];
+                if(x<0||x>=w || y<0||y>=h) continue;
+                if(table[y][x] == '#') cnt++;
+            }
+        }
+        table[i][j] = char(cnt + '0');
+    }}
+    rep(i,0,h){rep(j,0,w){
+        cout << table[i][j];
+    }
+    cout << endl;}
 }
